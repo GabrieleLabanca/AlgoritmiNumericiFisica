779134962e8aafd0e67d58d8df57ccb4fd32c56f
GabrieleLabanca <gabrielelabanca@gmail.com>
2017-05-26 18:10:08 +0200

    zeros with Lennard-Jones working

--- include/lib_zero.h
+++ include/lib_zero.h
@@ -159,141 +159,35 @@ double zero_newton( double (*f)(double x), double (*f_der)(double x),
   ifail = 1;
   int N = 50;
   bool xcheck, ycheck;
-  double fold, fnew, dfold, dfnew;
+  double xn, fn, dfn;
   double deltax, deltay;
-  double xold, xnew;
 
-  xold = est0;
+
+  xn = est0;
      
-  
-  fold = f(xold);
-  dfold = f_der(xold);
-
-  
-   
+  fn = f(xn);
+  dfn = f_der(xn);
+  deltax = xn;
+  deltay = fn;
 
   for(int i=0; i<N; i++){
-    xnew = xold - ( fold / dfold );
-    //cerr << " xnew = " << setprecision(11) << xnew << endl;
-     
+    xn = xn - ( fn / dfn );
+    fn = f(xn);
         
-    fnew = f(xnew);
-    
-    
-    deltax = xnew - xold;
-    deltay = fnew - fold;
-    
-
-    
-      
+    deltax -= xn;
+    deltay -= fn;
+          
     xcheck = sqrt(deltax*deltax) < x_accuracy;
     ycheck = sqrt(deltay*deltay) < y_accuracy;
     if( xcheck or ycheck ){
       ifail = 0;
-      return xnew;
-         
+      return xn;
     }
-
-    fold = fnew;
-    //dfnew = f_der(xnew);
-    //dfold = dfnew
-    dfold = f_der(xnew);
-    
-
   }
   
 
   // fail: return the old value
-  return xold;
+  return xn;
 }
 
 #endif
-
-/*
-
-
-double fexp_m(double x)
-{
-  return exp(-x) - x;
-}
-
-double fexp_m_der(double x)
-{
-  return -exp(-x) - 1;
-}
-
-
-double legendre(int n, double x)
-{
-  double y;
-
-  if(n == 0) return y = 1;
-  else if(n == 1) return y = 0;
-  else y = 1/double(n+1) * ( (2*n+1)*x*legendre(n,x) - n*legendre(n-1,x)     );
-  
-  return y;
-}
-
-//double d_legendre(int n, 
-
-
-
-int main()
-{
-  double x1;
-  double x2;
-  cerr << "Insert begin and end of interval:\n";
-  cin >> x1 >> x2;
-
-  
-
-  int tot_fail;
-  //////////////////////////////////////////////////////////
-  // Not convenient to iterate poorman/knuth alternance
-  int pm_fail;
-  int kn_fail;
-  poorman_bracketing(fexp_m, x1, x2, tot_fail);
-  pm_fail = tot_fail;
-  //while(pm_fail == 1){
-  if(pm_fail == 1) {
-    knuth_bracketing(fexp_m, x1, x2, tot_fail);
-    kn_fail = tot_fail;
-  }
-  //////////////////////////////////////////////////////////
-
-  // exit if no zero found
-  if(tot_fail == 1){
-    cerr << "!no zero found!\n";
-    return 1;
-  }
-
-  
-  cerr << "Bracketed interval:\n";
-  cerr << setw(20) << left << x1 << x2 << endl;
- 
-   
-
-  // zero_bisection( double (*f) (double x), double x1, double x2,
-  //   double x_accuracy, double y_accuracy, int &ifail)
-  double x_acc, y_acc, x_zero;
-  cerr << "Insert desired accuracy in x and f(x)\n";
-  cin >> x_acc >> y_acc;
-
-  x_zero = zero_bisection(fexp_m,x1,x2,0.001,0.001,tot_fail);
-  //x_zero = zero_regulafalsi(fexp_m,x1,x2,0.001,0.001,tot_fail);
-
-  cerr << "rough zero = " << setprecision(12) <<  x_zero << endl;
-  
-  x_zero = zero_newton(fexp_m,fexp_m_der,x1,x2,x_zero,x_acc,y_acc,tot_fail);
-  cerr << "Zero of the function is in " << x_zero << endl;
-  cerr << "Value of the function in this zero is " << setprecision(9) << fexp_m(x_zero) << endl;
-  
-
-
-
-
-  return 0;
-}
-*/
-
-
---
+++ prg/04_integrals/diatomic/diatomic.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+#include "lib_zero.h"
+#include "quadrature.h"
+#include "phys_constants.h"
+
+using namespace std;
+
+
+double eps; // eps = E/u0
+double f_lennard_jones(double x) // x= R/a
+{
+  double t = x*x*x*x*x*x;
+  return eps - 4.*( 1./t - 1 )/t;
+}
+
+double d_f_lennard_jones(double x)
+{
+  double t6 = x*x*x*x*x*x;
+  double t7 = x*t6;
+  return -24.*(1. - 2./t6)/t7;
+}
+
+double sqrt_f_lennard_jones(double x)
+{
+  return sqrt(f_lennard_jones(x));
+}
+int n;
+double gam;
+double xacc, yacc, quad_precision;
+double action(double energy)
+{
+  eps = energy;
+  int ifail;
+  double x_MAX = 7;
+  double dx = 0.1;
+  int N_interval = int(x_MAX/dx);
+  double x1, x2;
+  double z1, z2;
+
+  //cerr << "epsilon = " << eps << endl;
+  // first half
+  x1 = 1;
+  x2 = pow(2.,1./6.) - 0.001;
+  z1 = zero_bisection(f_lennard_jones,x1,x2,.001,.001,ifail);
+  z1 = zero_newton(f_lennard_jones,d_f_lennard_jones,x1,x2,z1,xacc,yacc,ifail);
+
+  //second half
+  x1 = pow(2.,1./6.) + 0.001;
+  x2 = x_MAX;
+  z2 = zero_bisection(f_lennard_jones,x1,x2,.001,.001,ifail);
+  z2 = zero_newton(f_lennard_jones,d_f_lennard_jones,x1,x2,z2,xacc,yacc,ifail);
+  //if(ifail == 0) cerr << "ZERO FOUND" << endl;
+  // cerr << "zeros: "
+  //     << setprecision(9) <<  z1 << " ; "
+  //     << setprecision(9) <<  z2 << endl;
+  return 2*gam*quad(sqrt_f_lennard_jones,z1,z2,quad_precision,'g') - 2*M_PI*(n+0.5); 
+}
+
+int main()
+{
+  cerr << "Insert desired accuracy in x and y:\n";
+  cin >> xacc >> yacc;
+
+  cerr << "Insert accuracy in quadrature method:\n";
+  cin >> quad_precision;
+  
+  cerr << "Insert gamma:\n";
+  cin >> gam;
+  for(n=0;n<9;n++){ // wkb method
+    int efail;
+    double e1, e2, e0;
+    double dE = 0.01;
+    int N_steps = 1./dE;
+    for(int en=0.; en<N_steps-1; en++){
+      e1 = -1. + en*dE;
+      e2 = e1 + dE;
+      poorman_bracketing(action,e1,e2,efail);
+      if(efail == 0){
+	e0 = zero_bisection(action,e1,e2,xacc,yacc,efail);
+	cerr << "n = " << n 
+	     << " -> zero in " << e0 << endl;
+      }
+    }
+  }
+
+  return 0;
+}
