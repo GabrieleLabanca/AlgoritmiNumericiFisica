12931c8ae05ef64ddbe4afea88e2e34a2b07ea66
GabrieleLabanca <gabrielelabanca@gmail.com>
2017-05-29 16:35:35 +0200

    cfr harmonic working

--- prg/04_integrals/diatomic/diatomic.cc
+++ prg/04_integrals/diatomic/diatomic.cc
@@ -30,23 +30,34 @@ double d_f_lennard_jones(double x)
   return -24.*(1. - 2./t6)/t7;
 }
 
-
-
-double sqrt_f(double(*V)(double x))
+/*
+double a, b, c;
+double parabola(double x)
 {
-  return sqrt(V(x));
+  return a*x*x + b*x + c;
 }
+
+double d_parabola(double x)
+{
+  return 2*a*x + b;
+}
+*/
+double sqrt_lennard_jones(double x)
+{
+  return sqrt(f_lennard_jones(x));
+}
+
 
 int n;
 double gam;
 double xacc, yacc, quad_precision;
-double action(double energy, double (*V)(double x), double (*dV)(double x))
+double action(double energy)//, double (*V)(double x), double (*dV)(double x))
 {
   eps = energy;
   int ifail;
   double x_MAX = 7;
-  double dx = 0.1;
-  int N_interval = int(x_MAX/dx);
+  //double dx = 0.1;
+  //int N_interval = int(x_MAX/dx);
   double x1, x2;
   double z1, z2;
 
@@ -54,27 +65,30 @@ double action(double energy, double (*V)(double x), double (*dV)(double x))
   // first half
   x1 = 1;
   x2 = pow(2.,1./6.) - 0.001;
-  z1 = zero_bisection(V,x1,x2,.001,.001,ifail);
-  z1 = zero_newton(V,dV,x1,x2,z1,xacc,yacc,ifail);
+  z1 = zero_bisection(f_lennard_jones,x1,x2,.001,.001,ifail);
+  z1 = zero_newton(f_lennard_jones,d_f_lennard_jones,x1,x2,z1,xacc,yacc,ifail);
 
   //second half
   x1 = pow(2.,1./6.) + 0.001;
   x2 = x_MAX;
-  z2 = zero_bisection(V,x1,x2,.001,.001,ifail);
-  z2 = zero_newton(V,dV,x1,x2,z2,xacc,yacc,ifail);
+  z2 = zero_bisection(f_lennard_jones,x1,x2,.001,.001,ifail);
+  z2 = zero_newton(f_lennard_jones,d_f_lennard_jones,x1,x2,z2,xacc,yacc,ifail);
   //if(ifail == 0) cerr << "ZERO FOUND" << endl;
   // cerr << "zeros: "
   //     << setprecision(9) <<  z1 << " ; "
   //     << setprecision(9) <<  z2 << endl;
-  return 2*gam*quad(sqrt_f(V),z1,z2,quad_precision,'g') - 2*M_PI*(n+0.5); 
+  return 2*gam*quad(sqrt_lennard_jones,z1,z2,quad_precision,'g') - 2*M_PI*(n+0.5); 
 }
 
-double action_LJ(double E)
+double parabola_eigenvalues(int q)
 {
-  return action(E,V,dV);
+  double x0 = pow(2.,1./6.);
+  double x8 = x0*x0*x0*x0 * x0*x0*x0*x0;
+  double x14 = x8 * x0*x0 * x0*x0 * x0*x0;
+  return -1 + (q+0.5)*2*sqrt(2)/gam*sqrt(6)*sqrt( 26/x14 - 7/x8 );
 }
 
-double action_par
+
 
 int main()
 {
@@ -86,7 +100,12 @@ int main()
   
   cerr << "Insert gamma:\n";
   cin >> gam;
-  for(n=0;n<9;n++){ // wkb method
+  cerr << setw(5) << left << "n"
+       << setw(15) << left << "zero"
+       << setw(15) << left << "harmonic"
+       << setw(15) << left << "sigma"
+       << endl;
+   for(n=0;n<9;n++){ // wkb method
     int efail;
     double e1, e2, e0;
     double dE = 0.01;
@@ -97,8 +116,12 @@ int main()
       poorman_bracketing(action,e1,e2,efail);
       if(efail == 0){
 	e0 = zero_bisection(action,e1,e2,xacc,yacc,efail);
-	cerr << "n = " << n 
-	     << " -> zero in " << e0 << endl;
+	cerr << setw(5) << left <<  n
+	     << setprecision(10) << setw(15) << left  << e0
+	  //<< setw(10) << left
+	     << setprecision(10) << setw(15) << left << parabola_eigenvalues(n)
+	     << setw(15) << left << setprecision(2) <<  abs((e0 - parabola_eigenvalues(n))/e0)
+	     << endl;
       }
     }
   }
